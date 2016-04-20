8595b1250ace4d679d2fa67ea0d0f54999fcc38c
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2016-04-20 15:18:35 +0100

    fix

--- Quiet Drive/Assets/Scripts/PlayerController.cs
+++ Quiet Drive/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace Assets.Scripts
+class PlayerController : MonoBehaviour
 {
-    class PlayerController
+    void Start()
     {
+
+    }
+
+    void Update()
+    {
+
     }
 }
