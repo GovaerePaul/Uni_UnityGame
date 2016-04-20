8180b637f276d787a4923141b3aeb64e4a642a77
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2016-04-20 15:43:26 +0100

    start that over

--- Quiet Drive/Assets/Scripts/PlayerController.cs
+++ Quiet Drive/Assets/Scripts/PlayerController.cs
@@ -1,55 +1,15 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
+using System.Collections;
 
+public class PlayerController : MonoBehaviour {
 
-public class Boundary
-{
-    public float xMin, xMax, zMin, zMax;
-}
-
-class PlayerController : MonoBehaviour
-{
-    [SerializeField]
-    private float engineForce = 10;
-    private float turnForce = 5;
-
-    void Start()
-    {
-
-    }
-
-    private Vector3 inputToForce()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        return new Vector3();
-    }
-
-    public float speed;
-    public float tilt;
-    public Boundary boundary;
-
-    void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rigidbody.velocity = movement * speed;
-
-        rigidbody.position = new Vector3
-        (
-            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
-            0.0f,
-            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
-        );
-
-        rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt);
-    }
-
-void Update()
-    {
-
-    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 }
