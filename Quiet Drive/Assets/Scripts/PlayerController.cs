8faa341a8699e033faef8854a739b47f88d22986
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2016-04-20 15:49:38 +0100

    :(

--- Quiet Drive/Assets/Scripts/PlayerController.cs
+++ Quiet Drive/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+
+    
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+    {
 	
 	}
 }
